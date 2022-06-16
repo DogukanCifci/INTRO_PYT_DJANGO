7af8177ac420ccd593a49c11d6be80223a60370a
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-06-16 15:27:01 +0200

    Entfernung berechnet in verschieden Einheiten

---
+++ intr_python_django/entfernung.py
@@ -0,0 +1,27 @@
+#in 10^6 
+entfernungen  = {
+    'Merkur' : 58, 
+    'Venus' : 108,
+    'Erde' : 150,
+    'Mars' : 228,
+    'Jupiter' :778,
+    'Saturn' :1427,
+    'Uranus' :2884,
+    'Neptun' :4509
+}
+
+
+umrechnungen = {
+    'km' : 1,
+    'm' : 1000,
+    'dm' : 10000,
+    'cm' : 100000
+}
+
+angefragte_planet = input('Bitte geben Sie den Namen des Planeten : ').title()
+angefragte_einheit = input('In welcher Einheit soll die Entefernung angezeigt werden? : ').lower()
+
+ergebnis = entfernungen[angefragte_planet] * 1000000 * umrechnungen[angefragte_einheit]
+
+print(f'Der Planet {angefragte_planet} hat eine Entfernung von {ergebnis} {angefragte_einheit} zur Sonne.')
+print('Hello')
