2e9b9b2a0b520ed5b7eed5aa30a625b5b7dc7805
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-06-21 09:27:57 +0200

    drawing method with 'turtle' added

---
+++ files_coursera/draw_turtle.py
@@ -0,0 +1,21 @@
+from turtle import *
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+
+#or 
+
+from turtle import *
+for i in range(4):
+    forward(100)
+    right(90)
+
+
+#at the end it is writed 2 times 
+#the both are same codes
