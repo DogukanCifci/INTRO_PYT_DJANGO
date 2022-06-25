4bfd70b76344218d56d4f72c8babf0d6b6583ce9
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-06-25 14:25:42 +0200

    new method added(the mostfrequent_mail adres)

---
+++ python_solving methods++/find_the_most_frequent_mail.py
@@ -0,0 +1,18 @@
+fhand = open("mbox-short.txt", "r")
+my_dict = dict()
+for line in fhand :
+    if not line.startswith('From ') :
+        continue
+    line = line.split()
+    words = line[1]
+    my_dict[words] = my_dict.get(words, 0) + 1
+print(my_dict)
+    
+largest = -1            #Bu metod cok faydali UNUTMAAAAA!!!!!!
+the_word = None
+for k, v in my_dict.items() :
+    if  v > largest :
+        largest = v
+        the_word = k
+
+print(the_word, largest)
