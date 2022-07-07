152bb66b0915ee802deec5a8e758355dfef672b6
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-07 15:12:37 +0200

    matris added

---
+++ numpy/matrix.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+X = np.array([[1,0],[0,1]])
+
+#x matrixi :
+# [1,0]         #bir parantez icindeymis gibi düsün.Bildigimiz matrix
+# [0,1]
+
+#Ilk liste ilk satir, 2. liste 2. satir
+
+
+Y = np.array([[2,4],[1,2]])
+# Y MATRIXI ;
+#[2,4]
+#[1,2]
+
