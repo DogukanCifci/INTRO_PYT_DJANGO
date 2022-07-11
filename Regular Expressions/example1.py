ec6019cbaff065e42858bdc5ce13181366678649
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-11 13:10:01 +0200

    to find numbers in a text

---
+++ Regular Expressions/example1.py
@@ -0,0 +1,13 @@
+import re 
+fhand = open("/Users/dogukan/Desktop/INTRO_PYT_DJANGO/Regular Expressions/mbox-short.txt", "r")
+numlist = list()
+for line in fhand :
+    line = line.rstrip()
+    stuff = re.findall("^X-DSPAM-Confidence: ([0-9.]+)", line)          #[0-9.] . konulmasi önemli yoksa 0 verir. en büyük sayi 0 dan kücük oldugu icin
+
+    if len(stuff) != 0 : 
+        num = float(stuff[0])
+        numlist.append(num)
+    else : 
+        continue
+print("Maksimum = ", max(numlist))
