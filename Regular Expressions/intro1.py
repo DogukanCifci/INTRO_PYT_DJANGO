b599fe54c791863f339753129dfecb086694ea64
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-11 11:57:37 +0200

    re.search() and re.findall() added

--- Regular Expressions/intro1.py
+++ Regular Expressions/intro1.py
@@ -4,3 +4,33 @@ for lines in fhand :
     lines =lines.rstrip()
     if lines.startswith('From: ') :
         print(lines)
+
+
+
+#Yerine 
+
+
+import re 
+hand = open("/Users/dogukan/Desktop/Regular Expressions/mbox-short.txt", "r")
+
+for line in hand :
+    line = line.rstrip()
+    if re.search('^From: ', line) :         #Birinci yere sartimiz, ikinci yerede nerde aradigimizi yaziyoruz.
+        print(line)
+
+
+
+#######  ^ isareti startswith anlamina gelmekte
+
+
+import re 
+fhand = open("/Users/dogukan/Desktop/Regular Expressions/mbox-short.txt", "r")
+
+for line in fhand :
+    line = line.rstrip()
+    if re.search("^X-\S+:", line) : 
+        print(line)                     #\S bosluk olmayan herhangi birkarakter anlamina geliyor.
+                                        #+ KENDINDEN BIR ÖNCEKI SARTI SAGLAYAN SEYLER ANLAMINA GELIYOR.
+#YANI BUNUN ANLAMI X- ILE BASLA. DAHA SONRA BOSLUK OLMADAN DEVAM ET : KARAKTERI GELENE KADAR.
+#CIKTIYA BAKARSA K: YA KADAR HICBIR BOSLUK YOK VE HEPSI X- ILE BASLAMISTIR
+        
