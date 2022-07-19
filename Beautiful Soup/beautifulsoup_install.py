b5f560e1583f6bb07631b1a1740bc97a431b178d
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-19 17:39:18 +0200

    logic of import method added
    with Screen Shoot

--- Beautiful Soup/beautifulsoup_install.py
+++ Beautiful Soup/beautifulsoup_install.py
@@ -1,11 +1,22 @@
+from cgitb import html
 import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 import ssl
 
+
+"""
+1-Web sitesine baglan
+2- Kaynak kodunu al
+3- BS modülü ile html kodlarini parcala
+"""
 
 #Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+
+
+
 
 
 
