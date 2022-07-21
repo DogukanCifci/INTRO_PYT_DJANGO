402ce8b5d14f8333a6726f3e9168a30f7ca90993
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-21 15:10:26 +0200

    filmname-ratingexporting from a website example

--- sockets/week3_task.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-my_url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
-
-R = requests.get(my_url)            #Burada url yi cektik
-
-print(R.text)           #Cektigimiz linkin bütün HTML Kodlarini almis olduk.
