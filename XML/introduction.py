6d49dbd8170bd19051deca83533e989c18b71fe4
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-23 12:55:24 +0200

    xml basic logic added

---
+++ XML/introduction.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET 
+
+data = '''<person>
+    <name>Chuck</name>
+    <phone type="intl">
+        +1 734 303 4456
+    </phone>
+    <email hide="yes"/>
+    <date>09.02.1985</date>
+    </person>'''
+
+tree = ET.fromstring(data)          #Tirnak icindeki bütün veriler tek bir data ordan import ettigimiz kütüphane yardimiyla veriyi cekiyoruz.
+
+print('Name : ', tree.find('name').text)            #Burda da veri icindeki istedigimiz veriyi özelligini belirterek aliyoruz.
+print('Attr : ', tree.find('email').get('hide'))    #Burda icerdeki hide özeliginde ne yazdigini getirir.
+print('Phone Type :', tree.find('phone').get('type'))   #Burdaki cikti da intl olacaktir. 
+print('Number : ', tree.find('phone').text.lstrip())    #lstrip yapma sebebim numara asagida gözüküyordu.
+print('Date : ', tree.find('date').text)
+
+####Eger tag icindeki bir özelligi almak istiyorsak .get ile ---- Tag arasindaki yaziyi almak istiyorsak .text ile aliyoruz.
