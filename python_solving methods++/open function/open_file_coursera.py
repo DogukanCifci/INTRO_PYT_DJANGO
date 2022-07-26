bf5918a188beabb1a5e689e594fd1e11bad8e92b
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-07-26 11:17:32 +0200

    read(), readline(), readlines() added

--- python_solving methods++/open function/open_file_coursera.py
+++ python_solving methods++/open function/open_file_coursera.py
@@ -1,17 +1,20 @@
 #FORMAT ---> handle = open(filename,mode) 
 
-fhand = open("/Users/dogukan/Desktop/python_kendi_calisma/python_Denemeler/romeo.txt", 'r')
+fhand = open("/Users/dogukan/Desktop/INTRO_PYT_DJANGO/python_solving methods++/open function/romeo.txt", 'r')
 print(fhand)
 count = 0
 for words in fhand :
+    words = words.rstrip()
     count += 1
-    print(words)
     print(count)
+    print(words)
+    print('*'*5)
+
     print(words[:10])
+    print('-'*5)
 
 
-
-dosya = open("/Users/dogukan/Desktop/python_kendi_calisma/python_Denemeler/romeo.txt", 'r')
+dosya = open("/Users/dogukan/Desktop/INTRO_PYT_DJANGO/python_solving methods++/open function/romeo.txt", 'r')
 inp = dosya.read()      #!!!!TÜM SATIRLARI YAN YANA KOYAR.\n bir karakter olarak alir.
 print(len(inp))
 print(inp[:50])            #BURDA DA TEK SATIRDAYKEN DILIMLEME ILE ISTEDIGIMIZE KADAR ALABILIRZ
